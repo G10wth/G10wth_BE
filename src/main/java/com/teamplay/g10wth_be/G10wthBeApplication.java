0cc974d4bd1e20ca29d657cab565ef950a6dd5ed
hexter31376 <zwonyoung33@gmail.com>
2025-05-21 04:20:00 +0900

    initial commit

---
+++ src/main/java/com/teamplay/g10wth_be/G10wthBeApplication.java
@@ -0,0 +1,13 @@
+package com.teamplay.g10wth_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G10wthBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G10wthBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teamplay/g10wth_be/G10wthBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamplay.g10wth_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G10wthBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
