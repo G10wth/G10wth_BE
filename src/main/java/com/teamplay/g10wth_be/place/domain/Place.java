5d364257ad909af4b8a43ccfb95da85a6a4eb5ed
hexter31376 <zwonyoung33@gmail.com>
2025-05-21 05:30:34 +0900

    [FEAT]: place 생성 #1

---
+++ src/main/java/com/teamplay/g10wth_be/place/domain/Place.java
@@ -0,0 +1,50 @@
+package com.teamplay.g10wth_be.place.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Table(name = "place")
+public class Place {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "place_id") // 명확한 테이블 구분을 위한 임의의 이름 추가
+    private Long id;
+
+    @Column(nullable = false)
+    private String name; // not null
+
+    private String category;
+
+    @Column(nullable = false)
+    private String address; // not null
+
+    @Column(nullable = false)
+    private Double latitude; // not null
+
+    @Column(nullable = false)
+    private Double longitude; // not null
+
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Member createdBy; //TODO: Member로 교체
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
